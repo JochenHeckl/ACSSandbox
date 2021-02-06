a7c91833e86eefc811df83db1b88d16a4d2f89a3
jochen <jochen.heckl@gmail.com>
2021-02-06 13:41:23 +0100

    Improves client side Jitter.
    Look ahead parameters tweaked.
    Position Lerp introduced to client view.

--- Samples~/SimpleExample/Scripts/Client/Unit/ClientUnitView.cs
+++ Samples~/SimpleExample/Scripts/Client/Unit/ClientUnitView.cs
@@ -1,16 +1,21 @@
 using de.JochenHeckl.Unity.ACSSandbox.Common;
 
+using UnityEngine;
+
 namespace de.JochenHeckl.Unity.ACSSandbox.Client
 {
 	public class ClientUnitView : UnitView
 	{
+		public float maxLerpDelaySec = 0.1f;
 		public ClientUnitData UnitData { get; set; }
 
 		public void Update()
 		{
 			if ( UnitData != null )
 			{
-				transform.position = UnitData.Position;
+				var lerpFactor = Mathf.Min( 1.0f, (Time.deltaTime / maxLerpDelaySec) );
+				
+				transform.position = Vector3.Lerp( transform.position, UnitData.Position, lerpFactor );
 				transform.rotation = UnitData.Rotation;
 			}
 		}
