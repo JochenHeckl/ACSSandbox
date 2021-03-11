2499cba5567aeeeb289b3a453d0aec1bc41ca102
jochen <jochen.heckl@gmail.com>
2021-03-11 17:57:29 +0100

    Adds Unapply Build Configuration after build

--- Runtime/Editor/BuildOperations.cs
+++ Runtime/Editor/BuildOperations.cs
@@ -12,30 +12,47 @@ namespace de.JochenHeckl.Unity.ACSSandbox.Editor
 	{
 		public static BuildReport Build( BuildConfiguration configuration )
 		{
-			ApplyBuildConfiguration( configuration );
+			UnapplyBuildConfigurationData unapplyBuildConfigurationData = null;
 
-			var buildOptions = new BuildPlayerOptions()
+			try
 			{
-				assetBundleManifestPath = configuration.assetBundleManifestPath,
-				extraScriptingDefines = configuration.extraScriptingDefines,
-				locationPathName = configuration.targetPath,
-				options = configuration.buildOptions,
-				scenes = configuration.scenes.Select( x => AssetDatabase.GetAssetPath( x ) ).ToArray(),
-				target = configuration.target,
-				targetGroup = configuration.targetGroup
-			};
+				unapplyBuildConfigurationData = ApplyBuildConfiguration( configuration );
 
-			var buildReport = BuildPipeline.BuildPlayer( buildOptions );
+				var buildOptions = new BuildPlayerOptions()
+				{
+					assetBundleManifestPath = configuration.assetBundleManifestPath,
+					extraScriptingDefines = configuration.extraScriptingDefines,
+					locationPathName = configuration.targetPath,
+					options = configuration.buildOptions,
+					scenes = configuration.scenes.Select( x => AssetDatabase.GetAssetPath( x ) ).ToArray(),
+					target = configuration.target,
+					targetGroup = configuration.targetGroup
+				};
 
-			Debug.Log( $"Build finished with result {buildReport.summary.result}." +
-				$" {buildReport.summary.totalErrors} Errors." +
-				$" {buildReport.summary.totalWarnings} Warnings." );
+				Directory.Delete( Path.GetDirectoryName( configuration.targetPath ), true );
 
-			return buildReport;
+				var buildReport = BuildPipeline.BuildPlayer( buildOptions );
+
+				Debug.Log( $"Build finished with result {buildReport.summary.result}." +
+					$" {buildReport.summary.totalErrors} Errors." +
+					$" {buildReport.summary.totalWarnings} Warnings." );
+
+				return buildReport;
+			}
+			finally
+			{
+				UnapplyBuildConfiguration( unapplyBuildConfigurationData );
+			}
 		}
 
-		private static void ApplyBuildConfiguration( BuildConfiguration configuration )
+		private static UnapplyBuildConfigurationData ApplyBuildConfiguration( BuildConfiguration configuration )
 		{
+			var unapplyBuildConfigurationData = new UnapplyBuildConfigurationData()
+			{
+				TargetGroup = configuration.targetGroup,
+				ActiveIncludes = PlayerSettings.GetScriptingDefineSymbolsForGroup( configuration.targetGroup )
+			};
+
 			PlayerSettings.productName = configuration.projectName;
 
 			PlayerSettings.SetScriptingBackend( configuration.targetGroup, configuration.scriptingImplementation );
@@ -43,6 +60,20 @@ namespace de.JochenHeckl.Unity.ACSSandbox.Editor
 
 			var defines = string.Join( ";", configuration.buildDefines.Distinct().ToArray() );
 			PlayerSettings.SetScriptingDefineSymbolsForGroup( configuration.targetGroup, defines );
+
+			return unapplyBuildConfigurationData;
+		}
+
+		private static void UnapplyBuildConfiguration( UnapplyBuildConfigurationData unapplyBuildConfigurationData )
+		{
+			if ( unapplyBuildConfigurationData == null )
+			{
+				return;
+			}
+
+			PlayerSettings.SetScriptingDefineSymbolsForGroup(
+				unapplyBuildConfigurationData.TargetGroup,
+				unapplyBuildConfigurationData.ActiveIncludes );
 		}
 	}
 }
---
+++ Runtime/Editor/UnapplyBuildConfigurationData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEditor;
+
+namespace de.JochenHeckl.Unity.ACSSandbox.Editor
+{
+	public class UnapplyBuildConfigurationData
+	{
+		public string ActiveIncludes { get; set; }
+		public BuildTargetGroup TargetGroup { get; set; }
+	}
+}
