423c3c23bceae981892a09673bd4ed4b9dd43e18
jochen <jochen.heckl@gmail.com>
2024-02-26 09:59:09 +0100

    Adds Build workflow

---
+++ ACSSandbox.Unity/Assets/Unity BuildConfig/Editor/ACSSandboxBuild.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using BuildConfig;
+using UnityEditor;
+
+namespace ACSSandbox
+{
+    public static class ACSSandboxBuild
+    {
+        [MenuItem("Build/Build Server")]
+        public static void BuildServer()
+        {
+            var buildConfigGuid =
+                AssetDatabase.FindAssets("Server.Release").SingleOrDefault()
+                ?? throw new InvalidOperationException("Server.Release.asset not found");
+
+            var buildConfigPath = AssetDatabase.GUIDToAssetPath(buildConfigGuid);
+            var buildConfig = AssetDatabase.LoadAssetAtPath<BuildConfiguration>(buildConfigPath);
+
+            Build.BuildWithConfig(buildConfig);
+        }
+    }
+}
