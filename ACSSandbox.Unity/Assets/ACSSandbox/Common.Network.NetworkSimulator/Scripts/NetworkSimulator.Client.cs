2512767ed44d0e942a70e0b07aca164634cc0679
jochen <jochen.heckl@gmail.com>
2024-10-25 13:04:11 +0200

    working async simulator and ruffles server

---
+++ ACSSandbox.Unity/Assets/ACSSandbox/Common.Network.NetworkSimulator/Scripts/NetworkSimulator.Client.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ACSSandbox.Common.Network.NetworkSimulator
+{
+    public partial class NetworkSimulator : INetworkClient
+    {
+        public async Task RunClientAsync(
+            string host,
+            int servicePort,
+            INetworkClientEventProcessor eventProcessor,
+            CancellationToken cancellationToken
+        )
+        {
+            Connections = new[] { NetworkId.Create() };
+
+            eventProcessor.HandleConnect();
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Yield();
+
+                while (serverMessages?.TryDequeue(out var message) ?? false)
+                {
+                    eventProcessor.HandleInboundData(message);
+                }
+            }
+        }
+
+        public void Send(ReadOnlySpan<byte> message, TransportChannel channel)
+        {
+            clientMessages.Enqueue(message.ToArray());
+        }
+    }
+}
---
+++ ACSSandbox.Unity/Assets/ACSSandbox/Common.Network.NetworkSimulator/Scripts/NetworkSimulator.cs
@@ -0,0 +1,21 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using ACSSandbox.Common.Network;
+using UnityEngine;
+
+namespace ACSSandbox.Common.Network.NetworkSimulator
+{
+    public partial class NetworkSimulator : MonoBehaviour
+    {
+        public IEnumerable<NetworkId> Connections { get; private set; }
+
+        private ConcurrentQueue<byte[]> clientMessages;
+        private ConcurrentQueue<byte[]> serverMessages;
+
+        void Awake()
+        {
+            clientMessages = new();
+            serverMessages = new();
+        }
+    }
+}
